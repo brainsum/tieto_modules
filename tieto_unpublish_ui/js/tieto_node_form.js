50cbef406d460b39161934a31e22ac3582bad625
mhavelant <mhavelant@brainsum.com>
2018-07-11 15:58:23 +0200

    TSP-461 | Don't display notification description when the corresponding action is not rendered.
    Took 4 minutes

--- tieto_unpublish_ui/js/tieto_node_form.js
+++ tieto_unpublish_ui/js/tieto_node_form.js
@@ -8,56 +8,68 @@
   Drupal.behaviors.tieto_node_forms = {
     attach: function (context, settings) {
       $('.ief-form .field--widget-jquery-ui-datepicker-timestamp')
-        .once('buttonUpdate')
-        .each(function(){
-          var dpWrapper = $(this);
-          $(this)
-            .find('input')
-            .attr('autocomplete', 'off')
-            .on('change', updateSubmitButton);
+      .once('buttonUpdate')
+      .each(function () {
+        var dpWrapper = $(this);
+        $(this)
+        .find('input')
+        .attr('autocomplete', 'off')
+        .on('change', updateSubmitButton);
 
-          function updateSubmitButton(e){
-            var good = true;
-            dpWrapper.find('input').each(function(){
-              if ($(this).val().length === 0) {
-                good = false;
-              }
-            });
-
-            if (good) {
-              dpWrapper
-                .closest('.ief-form')
-                .find('.ief-entity-submit:not(.scheduled)')
-                .addClass('scheduled');
-            } else {
-              dpWrapper
-                .closest('.ief-form')
-                .find('.ief-entity-submit.scheduled')
-                .removeClass('scheduled');
+        function updateSubmitButton(e) {
+          var good = true;
+          dpWrapper.find('input').each(function () {
+            if ($(this).val().length === 0) {
+              good = false;
             }
-          }
+          });
 
-          updateSubmitButton();
-        });
+          if (good) {
+            dpWrapper
+            .closest('.ief-form')
+            .find('.ief-entity-submit:not(.scheduled)')
+            .addClass('scheduled');
+          }
+          else {
+            dpWrapper
+            .closest('.ief-form')
+            .find('.ief-entity-submit.scheduled')
+            .removeClass('scheduled');
+          }
+        }
+
+        updateSubmitButton();
+      });
     }
+  };
+
+  function actionExists(selector) {
+    var notification = $(selector + ' .action-button--action-notification-wrapper');
+    var button = $(selector + ' .action-button--action-wrapper');
+
+    return notification.children().length > 0 && button.children().length > 0;
   }
 
-  $.fn.updateButtonHints = function(scheduledDates){
+  $.fn.updateButtonHints = function (scheduledDates) {
     var selectors = {
-      'scheduled_publish_date' : '.form-action-moderation-state-unpublished-button-wrapper',
-      'scheduled_unpublish_date' : '.form-action-moderation-state-unpublished-content-button-wrapper',
-      'scheduled_trash_date' : '.form-action-moderation-state-trash-button-wrapper',
+      'scheduled_publish_date': '.form-action-moderation-state-unpublished-button-wrapper',
+      'scheduled_unpublish_date': '.form-action-moderation-state-unpublished-content-button-wrapper',
+      'scheduled_trash_date': '.form-action-moderation-state-trash-button-wrapper',
     };
 
     var $notifElement, $buttonElement, pubSelector;
-    $.each(scheduledDates, function(index, item){
+    $.each(scheduledDates, function (index, item) {
       pubSelector = false;
-      $(selectors[index] + ' .action-description').html(item.text);
 
       $notifElement = $(selectors[index] + ' .action-button--action-notification-wrapper');
       $buttonElement = $(selectors[index] + ' .action-button--action-wrapper');
 
-      if (index == 'scheduled_publish_date') {
+      var actionDoesExist = actionExists(selectors[index]);
+      if (actionDoesExist) {
+        $(selectors[index] + ' .action-description').html(item.text);
+      }
+
+      if (index === 'scheduled_publish_date') {
         pubSelector = '.form-action-moderation-state-published-button-wrapper';
         $notifElement = $(pubSelector + ' .action-button--action-notification-wrapper');
         $buttonElement = $(pubSelector + ' .action-button--action-wrapper');
@@ -70,18 +82,19 @@
           $(pubSelector + '.scheduled').removeClass('scheduled');
         }
 
-        if ($notifElement.children().length > 0 && $buttonElement.children().length > 0){
+        if (actionDoesExist) {
           $notifElement.not('.action-button--hidden-action-wrapper').addClass('action-button--hidden-action-wrapper');
           $buttonElement.removeClass('action-button--hidden-action-wrapper');
         }
-      } else {
+      }
+      else {
         $(selectors[index] + ':not(.scheduled)').addClass('scheduled');
 
         if (pubSelector) {
           $(pubSelector + ':not(.scheduled)').addClass('scheduled');
         }
 
-        if ($notifElement.children().length > 0 && $buttonElement.children().length > 0){
+        if (actionDoesExist) {
           $notifElement.removeClass('action-button--hidden-action-wrapper');
           $buttonElement.not('.action-button--hidden-action-wrapper').addClass('action-button--hidden-action-wrapper');
         }
