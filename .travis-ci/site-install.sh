d31f7bc94841f8bc87149d9c3616737ff38f2456
mhavelant <mhavelant@brainsum.com>
2019-02-14 12:10:09 +0100

    TSP-778 | Add travis integration.

---
+++ .travis-ci/site-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Note: This should run inside docker.
+
+# Install composer dependencies and the site.
+composer config extra.patches-file "composer.patches.json" \
+  && composer config discard-changes true \
+  && composer require drupal/tieto_modules:2.x-dev \
+  && composer install -n \
+  && cd web \
+  && drush site-install --site-name="Test" --account-pass=123 --db-url=mysql://drupal:drupal@mariadb/drupal standard -y \
+  && drush en \
+    jquery_ui_datepicker \
+    tieto_general_ui \
+    tieto_link_customization \
+    tieto_tibr \
+    tieto_unpublish_ui -y \
+  && drush cr
