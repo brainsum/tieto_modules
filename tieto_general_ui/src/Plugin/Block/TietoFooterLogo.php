b1f993e3a6435428087cdb5fd8d746d5ce4f2454
mhavelant <mhavelant@brainsum.com>
2019-02-18 16:08:26 +0100

    TSP-781 | Add footer logo block.

---
+++ tieto_general_ui/src/Plugin/Block/TietoFooterLogo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\tieto_general_ui\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'TietoFooterLogo' Block.
+ *
+ * @Block(
+ *   id = "TietoFooterLogo",
+ *   admin_label = @Translation("Tieto footer logo"),
+ * )
+ */
+class TietoFooterLogo extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $imagePath = \drupal_get_path('module', 'tieto_general_ui') . '/images/logofooter.png';
+    return [
+      '#markup' => "<img src='$imagePath' alt='Tieto logo' />",
+    ];
+  }
+
+}
